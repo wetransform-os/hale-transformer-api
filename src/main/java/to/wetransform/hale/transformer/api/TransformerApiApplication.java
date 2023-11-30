fdb9fb7bc94aa6d0733f018d800d5b0681ad978f
Florian Esser <fe@wetransform.to>
2023-11-30 08:07:36 +0100

    chore(spotless): switch to palantir-java-format

--- src/main/java/to/wetransform/hale/transformer/api/TransformerApiApplication.java
+++ src/main/java/to/wetransform/hale/transformer/api/TransformerApiApplication.java
@@ -9,5 +9,4 @@ public class TransformerApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransformerApiApplication.class, args);
     }
-
 }
--- src/test/java/to/wetransform/hale/transformer/api/TransformerApiApplicationTests.java
+++ src/test/java/to/wetransform/hale/transformer/api/TransformerApiApplicationTests.java
@@ -7,7 +7,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TransformerApiApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
