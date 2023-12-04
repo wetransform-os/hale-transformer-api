9b7d7374cc93b508641177ab546ba1400806a928
Florian Esser <fe@wetransform.to>
2023-12-04 15:12:49 +0100

    refactor: add hale artifcats for transformation functionality
    SVC-1398

---
+++ src/main/java/to/wetransform/hale/transformer/Transformer.java
@@ -0,0 +1,13 @@
+package to.wetransform.hale.transformer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Transformer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Transformer.class);
+
+    void transform() {
+        LOG.info("Transforming...");
+    }
+}
---
+++ src/main/java/to/wetransform/hale/transformer/TransformerConfig.java
@@ -0,0 +1,6 @@
+package to.wetransform.hale.transformer;
+
+public class TransformerConfig {
+
+    // empty for now
+}
